9de742e9cfef69fea2340ed6eb67cc7769023c1b
Alexmin22 <yashi80@list.ru>
2023-01-23 18:01:38 +0300

    Добавлены 2 строки

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+    //Реализуйте generic-класс Pair, похожий на Optional, но содержащий пару элементов разных типов и не запрещающий
+    // элементам принимать значение null.
+    //Реализуйте методы getFirst(), getSecond(), equals() и hashCode(), а также статический фабричный метод of().
+    // Конструктор должен быть закрытым (private).
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Pair<Integer, String> p =  Pair.of(5, "0.56");
+
+        System.out.println(p.toString());
     }
+
+
 }
